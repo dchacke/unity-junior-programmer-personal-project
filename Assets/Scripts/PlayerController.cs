2f9d2b5350b674b78a69ba3c949f7e0eb7695768
Dennis Hackethal <dh@dennishackethal.com>
2023-12-22 15:32:49 -0700

    Implement basic movement from user input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.AddForce(Vector3.up * verticalSpeed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector3.right * horizontalInput * horizontalSpeed);
     }
 }
